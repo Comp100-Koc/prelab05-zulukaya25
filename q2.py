0948a192cfea68eb9a05d4a46519ac0936961a3f
zulukaya25 <zulukaya25@ku.edu.tr>
2026-04-01 21:35:14 +0300

    pl5

--- q2.py
+++ q2.py
@@ -1,5 +1,26 @@
 def remove_adjacent_duplicates(s):
-    '''
-    Given a string remove all the adjacent duplicate characters and return the string
-    '''
-    pass
+    
+    while adjacent_duplicates_exist(s):
+        result = ""
+        
+        for i in range(len(s)):
+            
+            if i < len(s)-1 and s[i] == s[i+1]:
+                result += s[i+2:] 
+                break
+            
+            else:
+                result += s[i]
+
+        s = result
+        
+    return s
+
+def adjacent_duplicates_exist(s):
+    
+    for i in range(len(s)-1):
+        
+        if s[i] == s[i+1]:
+            return True
+        
+    return False
