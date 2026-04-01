2e9e31220b2571ef515301826e608fa4019aaf08
zulukaya25 <zulukaya25@ku.edu.tr>
2026-04-01 21:17:49 +0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,5 +1,17 @@
 def longest_palindromic_substring(s):
-    """
-    Given a string find the longest palindromic substring
-    """
-    pass
+    
+    longest = ""
+    
+    for i in range(len(s)):
+        
+        for j in range(i + 1, len(s) + 1):
+            substring = s[i:j]
+            
+            if substring == substring[::-1] and len(substring) > len(longest):
+                longest = substring
+    
+    if len(longest) <= 1:
+        return ''
+    
+    return longest
+            
